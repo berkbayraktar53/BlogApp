a2c61052281ed6a7ba189858d5b66b26b8289718
Kerem Berk Bayraktar <keremberk53@gmail.com>
2022-03-13 14:34:05 +0300

    About page added

---
+++ BlogApp.WebUI/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.WebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _aboutService.GetAll();
+            return View(values);
+        }
+
+        public PartialViewResult SocialMedia()
+        {
+            return PartialView();
+        }
+    }
+}
